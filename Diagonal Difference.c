997190847eb0eb272f4f2b0425e532a77b26b14f
Jonathan Hernandez <helharatheband@gmail.com>
2016-09-04 21:24:09 -0700

    Update Diagonal Difference.c

--- Diagonal Difference.c
+++ Diagonal Difference.c
@@ -1,3 +1,15 @@
+/**
+ * 
+ * Given a square matrix of size , calculate the absolute difference between the sums of its diagonals.
+ * 
+ * Input Format
+ * The first line contains a single integer, . The next  lines denote the matrix's rows, with each line 
+ * containing space-separated integers describing the columns.
+ * 
+ * Output Format
+ * Print the absolute difference between the two sums of the matrix's diagonals as a single integer.
+ * 
+ */
 #include <math.h>
 #include <stdio.h>
 #include <string.h>
